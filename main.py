c964c09062af3105476c00d99086dbba8aa63c89
RaluElena05 <grosuralucaelena@yahoo.com>
2025-01-18 13:07:27 +0200

    calculator project

---
+++ main.py
@@ -0,0 +1,10 @@
+from calculator import adunare, scadere
+
+num1 = 10
+num2 = 5
+
+rezultat_adunare = adunare(num1, num2)
+rezultat_scadere = scadere(num1, num2)
+
+print(f"{num1} + {num2} = {rezultat_adunare}")
+print(f"{num1} - {num2} = {rezultat_scadere}")
